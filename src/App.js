37d1a9d70dc182d34e14f0fd29e685237a1cf00b
akhil1198 <akhilshridhar02@gmail.com>
2025-01-26 05:34:28 -0500

    ui bug

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const ExpenseTracker = () => {
 
 			<div className="max-w-3xl mx-auto p-4 bg-background">
 				<div className="text-center m-5 slex grid grid-cols-2">
-					<h1 className="text-3xl font-black text-button text-left">E-Tracker</h1>
+					<h1 className="text-3xl font-bold text-button text-left">E-Tracker</h1>
 					<div className="text-center slex grid grid-cols-2 gap-x-4">
 						<button type="button"
 							onClick={(() => handleViewTypeChange('Add'))}
